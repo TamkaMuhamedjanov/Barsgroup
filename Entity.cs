5e5a23d14743ce865d0a8074e968966da3287c00
TamkaMuhamedjanov <tima.muhamejanov@yandex.ru>
2022-04-04 17:17:13 +0300

    ex2

---
+++ Entity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Entity
+    {
+        public int Id;
+        public int ParentId;
+        public string Name;
+        public static Dictionary<int, List<Entity>> ToDictionary(List<Entity> list)
+        {
+            var dict = list
+                .GroupBy(item => item.ParentId)
+                .ToDictionary(g => g.Key, g => g.ToList());
+
+            return dict;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entity> Test = new List<Entity>()
+            {
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity"},
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"},
+                new Entity { Id = 6, ParentId = 1, Name = "Child of 1 entity"}
+            };
+            var Dict = Entity.ToDictionary(Test);
+            
+
+        }
+    }
+}
